646faa0055448f707d19a0d1f37a5bd57126521b
osondu dominion <domikdegreat@gmail.com>
2023-04-21 00:01:58 +0100

    File update: styling of Search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -44,7 +44,7 @@ const SearchBar = () => {
         
         />
 
-        <IconButton type="submit" sx={{ p:'10px', color: 'red', ml: {xs: '150px', md: '380px', sm:'380px'}, mt: {md:'-45px', xs: '-32px'}}}>
+        <IconButton type="submit" sx={{ p:'10px', color: 'red', ml: {xs: '150px', md: '380px', sm:'380px'}, mt: {md:'-45px', xs: '-30px'}}}>
             <Search />
         </IconButton>
     </Paper>
